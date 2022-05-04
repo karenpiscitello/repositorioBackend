635543de49f706c3f0a14c2de51c380a07bc4512
karenpiscitello <karen.piscitello23@gmail.com>
2022-05-04 20:26:30 -0300

    cambios

--- classUsuario.js
+++ classUsuario.js
@@ -1,29 +1,47 @@
 class Usuario {
 
-    constructor (nombre, apellido) {
+    constructor (nombre, apellido, libros, mascotas) {
         
         this.nombre = nombre;
         this.apellido = apellido;
-        //this.mascotas = mascotas || [];
-        //this.libros = libros || [];
+        this.mascotas = mascotas || [];
+        this.libros = libros || [];
     }
     
-    getFullName() {
+    getFullName = () =>{
         return `${nombre} + ${apellido}`;
     }
-}
-const usuario = new Usuario('Karen', 'Piscitello');//, { mascotas, libros })
+    addMascota(mascota){
+        this.mascotas.push = mascota;
+    }
+    countMascotas(){
+    
+    }
 
+    addBook(nombre, autor) {
+        this.libros.push({ 
+        nombre: nombre,
+        autor: autor
+    })
+    }
+
+    getBookNames(){
+        const nombresLibros = this.libros.map((libro) => libro.nombre)
+        return nombresLibros 
+    }
+    
+    
+}
+const usuario1 = new Usuario('Karen', 'Piscitello', [{nombre: "Harry Potter", autor: "JK Rowling"}], ["Perrito"]);
+usuario1.getFullName()
 console.log(getFullName());
 
-// const datosCompletos = (datos) => {
-//     const { nombre, apellido } = datos;
-//     return {
-//         nombre_completo: `${nombre} ${apellido}`
-//     }
-// }
+usuario1.addMascota("Oveja") 
+usuario1.addMascota("Gatito")
 
+console.log(Usuario.countMascotas())
 
+usuario1.addBook("El señor de los anillos", "J. R. R. Tolkien") 
+usuario1.addBook("Libro inventado", "Yo") 
 
-// const usuario = new Usuario('Karen', 'Piscitello', { mascotas, libros })
-// console.log(usuario)
+console.log( usuario1.getBookNames() )
